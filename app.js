625608807f8db7f1cef8b1697f47567d2f6a7de9
booybooy <bailey.peng@gmail.com>
2014-11-11 01:09:57 -0500

    Hello world with angularJS and bootstrap.

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bailey on 11/11/2014.
+ */
+
+(function() {
+    'use strict';
+    angular.module("helloworld", ["helloworld.controllers"]);
+    angular.module("helloworld.controllers", []);
+})();
---
+++ controller/helloWorldCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bailey on 11/11/2014.
+ */
+(function() {
+    'use strict';
+    angular.module("helloworld.controllers")
+        .controller("helloWorldCtrl", function() {
+            this.name = "Bailey";
+        });
+})();
